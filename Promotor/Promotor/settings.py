52c54f9d068aba0b3ab0ef8e6f25502ae9d4c7ba
krzysieksied <siedleckikrzysztof146@gmail.com>
2021-09-03 16:54:44 +0200

    first models

--- Promotor/Promotor/settings.py
+++ Promotor/Promotor/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,7 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'webapp.ProfilUser'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp'
 ]
 
 MIDDLEWARE = [
@@ -69,17 +69,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Promotor.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'new',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -112,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- Promotor/webapp/models.py
+++ Promotor/webapp/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+
+class ProfilUser(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(ProfilUser, on_delete=models.CASCADE, primary_key=True, related_name='Student')
+    name = models.CharField(max_length=250)
+    index = models.IntegerField()
+    email = models.EmailField(max_length=254)
+
+    def get_absolute_url(self):
+        return reverse('student_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(ProfilUser, on_delete=models.CASCADE, primary_key=True, related_name='Teacher')
+    name = models.CharField(max_length=250)
+    subject_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=254)
+    class_students = models.ManyToManyField(Student, through="StudentsInClass")
+
+    def get_absolute_url(self):
+        return reverse('teacher_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.name
+
+
+class StudentsInClass(models.Model):
+    teacher = models.ForeignKey(Teacher, related_name="class_teacher", on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, related_name="user_student_name", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.student.name
+
+    class Meta:
+        unique_together = ('teacher', 'student')
